63020f6033c3a6db3bc341b7ff793ccddbfd5e40
basti <spmbot123@gmail.com>
2012-12-08 00:35:24 +0100

    styling and new dashboard element

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,21 @@
 module ApplicationHelper
-    
+   
+  # sort columns
   def sortable(column, title = nil)  
     title ||= column
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"  
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end  
-  
+
+  #show the 5 most used tags: "tag_id" => post_count
+  def top_tags(most_used_tags)
+    Hash[*most_used_tags.sort_by { |k,v| -v }[0..5].flatten]
+  end
+
+  #needed in home, because i need the tag_id for the link, but a user wants more. e.g. name 
+  def get_tagname(tag_id)
+    Tag.find(tag_id) 
+  end
+
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,12 +3,20 @@ module PostsHelper
     post.tags.map { |t| t.name }.join(", ")
   end
 
+  #use nice strings in _show for create/update dates
   def create_date(post)
     " created at: " + post.created_at.strftime("%d %b. %Y") + " - " + post.created_at.strftime("%H:%M") 
   end
-
   def update_date(post)
     " updated at: " + post.updated_at.strftime("%d %b. %Y") + " - " + post.updated_at.strftime("%H:%M") 
+  end
+
+  #set a string and title(new) for submit button in _form
+  if @current_action == "edit" or "update"
+    @action_string = "update entry "
+  elsif @current_action == "new" or "create"
+    @action_string = "create entry"
+    @action_title = "new entry" 
   end
 
 end
