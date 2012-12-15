bb73d85762a9e038a35282d57550b05c4cb804c3
basti <spmbot123@gmail.com>
2012-12-15 13:31:02 +0100

    finished tooltipping to help the user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     title ||= column
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"  
-    link_to title, params.merge(:sort => column, :direction => direction), {:rel => 'tooltip', :data => { :delay => '800' }, :title => "click to sort this column ascending :: click again to sort this column descending"}, {:class => css_class}
+    link_to title, params.merge(:sort => column, :direction => direction), { :rel => 'tooltip', :data => { :delay => '800' }, :title => "click to sort this column ascending :: click again to sort this column descending", :class => css_class}
   end  
 
   #show the 5 most used tags: "tag_id" => post_count
@@ -18,18 +18,18 @@ module ApplicationHelper
     Tag.find(tag_id) 
   end
 
-  #remove field. used for new tag on post
+  #remove nested field. used for new tag on post
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :class => "icon-trash")
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", { :rel => 'tooltip', :data => { :delay => '800' }, :title => "remove row. tag will not be attached to entry.", :class =>  "icon-trash" } )
   end
 
-  #add field. used for new tag on post
+  #add nested field. used for new tag on post
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => "icon-plus")
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", { :rel => 'tooltip', :data => { :delay => '800' }, :title => "add new row to enter a new tag. tag will be attached to the entry.", :class => "icon-plus" } )
   end
 
 
