50c83297bc5635fdb2872a980e34aea5fffe2a57
basti <spmbot123@gmail.com>
2012-12-18 21:34:49 +0100

    goo.gl shortener in post/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,5 +33,12 @@ module ApplicationHelper
   end
 
 
+  #google url shortener. place your API key in: config/google_api_key
+  Google::UrlShortener::Base.api_key =  File.open('config/google_api_key').read
+  def shorten_url(post_id)
+    title = Post.find(post_id).title
+    url = Google::UrlShortener::Url.new(:long_url => title)
+    url.shorten! 
+  end
 
 end
