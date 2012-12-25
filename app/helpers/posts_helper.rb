e04711a593fbfdb43850fd3b9e2cb85f2c6b01ca
basti <spmbot123@gmail.com>
2012-12-25 21:53:56 +0100

    bring back the title (update, new, create) in post/form

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,12 +11,6 @@ module PostsHelper
     " updated at: " + post.updated_at.strftime("%d %b. %Y") + " - " + post.updated_at.strftime("%H:%M") 
   end
 
-  #set a string and title(new) for submit button in _form
-  if @current_action == "edit" or "update"
-    @action_string = "update entry "
-  elsif @current_action == "new" or "create"
-    @action_string = "create entry"
-    @action_title = "new entry" 
-  end
+
 
 end
