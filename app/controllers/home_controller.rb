122399b5741403a25d19c9b70d3ee2b7b35ef632
basti <spmbot123@gmail.com>
2012-12-26 13:41:27 +0100

    fallback if no post has a description for featured post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,11 @@ class HomeController < ApplicationController
     #last created posts
     @last_posts = Post.find(:all, :order => "id desc", :limit => 6)
     #value posts are defined with a description. sql: one character (_) followed by zero, one, or more characters (%)
+    #if no post has a description we just take all posts
     @value_posts =  Post.where( 'content like ?', '_%' )
+    if @value_posts.empty? 
+      @value_posts = Post.all
+    end  
     #random post from the value posts
     @featured_post = @value_posts.sort_by { rand }.first
    
