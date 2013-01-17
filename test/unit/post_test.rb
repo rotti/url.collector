874efade87541af42809023d1a80f3e502a7c29e
basti <spmbot123@gmail.com>
2013-01-17 10:40:16 +0100

    lately we start unit testing ... uggh

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #just a normal entry to posts
+  test "should create a new post with name, title and content" do
+    assert p = Post.new
+    assert_equal false, p.valid?, "should not be valid; Post.new seems to be broken"
+    p.name = "new test post"
+    p.title = "http://new.post.fu"
+    p.content = "some text that emulates our description"
+    assert_equal true, p.valid?, "all fields of the post should be valid"
+    assert_nothing_raised {p.save!}
+  end 
 end
